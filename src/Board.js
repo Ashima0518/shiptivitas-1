53bbfd06f7a7daad2085b8f58f156335a469b851
Ashima Sharma <154715063+Ashima0518@users.noreply.github.com>
2026-01-17 19:29:18 -0600

    frontend task 1 - complete

--- src/Board.js
+++ src/Board.js
@@ -21,33 +21,93 @@ export default class Board extends React.Component {
       complete: React.createRef(),
     }
   }
+
+  componentDidMount() {
+    this.drake = Dragula([
+      this.swimlanes.backlog.current,
+      this.swimlanes.inProgress.current,
+      this.swimlanes.complete.current
+    ]);
+
+    this.drake.on('drop', () => {
+      const nextClients = this.syncStateWithDOM();
+
+      this.drake.cancel(true);
+
+      this.setState({
+        clients: nextClients,
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    if (this.drake) this.drake.destroy();
+  }
+
+  syncStateWithDOM() {
+    const allClients = [
+      ...this.state.clients.backlog,
+      ...this.state.clients.inProgress,
+      ...this.state.clients.complete,
+    ];
+
+    const clientMap = {};
+    allClients.forEach((c) => {
+      clientMap[c.id] = c;
+    });
+
+    const getIdsFromLane = (laneRef) =>
+      Array.from(laneRef.current.children).map((el) => el.dataset.id);
+
+    const backlogIds = getIdsFromLane(this.swimlanes.backlog);
+    const inProgressIds = getIdsFromLane(this.swimlanes.inProgress);
+    const completeIds = getIdsFromLane(this.swimlanes.complete);
+
+    const backlog = backlogIds.map((id) => ({
+      ...clientMap[id],
+      status: 'backlog',
+    }));
+
+    const inProgress = inProgressIds.map((id) => ({
+      ...clientMap[id],
+      status: 'in-progress',
+    }));
+
+     const complete = completeIds.map((id) => ({
+      ...clientMap[id],
+      status: 'complete',
+    }));
+
+    return { backlog, inProgress, complete };
+  }
+
   getClients() {
     return [
-      ['1','Stark, White and Abbott','Cloned Optimal Architecture', 'in-progress'],
-      ['2','Wiza LLC','Exclusive Bandwidth-Monitored Implementation', 'complete'],
-      ['3','Nolan LLC','Vision-Oriented 4Thgeneration Graphicaluserinterface', 'backlog'],
-      ['4','Thompson PLC','Streamlined Regional Knowledgeuser', 'in-progress'],
-      ['5','Walker-Williamson','Team-Oriented 6Thgeneration Matrix', 'in-progress'],
-      ['6','Boehm and Sons','Automated Systematic Paradigm', 'backlog'],
-      ['7','Runolfsson, Hegmann and Block','Integrated Transitional Strategy', 'backlog'],
-      ['8','Schumm-Labadie','Operative Heuristic Challenge', 'backlog'],
-      ['9','Kohler Group','Re-Contextualized Multi-Tasking Attitude', 'backlog'],
-      ['10','Romaguera Inc','Managed Foreground Toolset', 'backlog'],
-      ['11','Reilly-King','Future-Proofed Interactive Toolset', 'complete'],
-      ['12','Emard, Champlin and Runolfsdottir','Devolved Needs-Based Capability', 'backlog'],
-      ['13','Fritsch, Cronin and Wolff','Open-Source 3Rdgeneration Website', 'complete'],
-      ['14','Borer LLC','Profit-Focused Incremental Orchestration', 'backlog'],
-      ['15','Emmerich-Ankunding','User-Centric Stable Extranet', 'in-progress'],
-      ['16','Willms-Abbott','Progressive Bandwidth-Monitored Access', 'in-progress'],
-      ['17','Brekke PLC','Intuitive User-Facing Customerloyalty', 'complete'],
-      ['18','Bins, Toy and Klocko','Integrated Assymetric Software', 'backlog'],
-      ['19','Hodkiewicz-Hayes','Programmable Systematic Securedline', 'backlog'],
-      ['20','Murphy, Lang and Ferry','Organized Explicit Access', 'backlog'],
+      ['1','Stark, White and Abbott','Cloned Optimal Architecture'],
+      ['2','Wiza LLC','Exclusive Bandwidth-Monitored Implementation'],
+      ['3','Nolan LLC','Vision-Oriented 4Thgeneration Graphicaluserinterface'],
+      ['4','Thompson PLC','Streamlined Regional Knowledgeuser'],
+      ['5','Walker-Williamson','Team-Oriented 6Thgeneration Matrix'],
+      ['6','Boehm and Sons','Automated Systematic Paradigm'],
+      ['7','Runolfsson, Hegmann and Block','Integrated Transitional Strategy'],
+      ['8','Schumm-Labadie','Operative Heuristic Challenge'],
+      ['9','Kohler Group','Re-Contextualized Multi-Tasking Attitude'],
+      ['10','Romaguera Inc','Managed Foreground Toolset'],
+      ['11','Reilly-King','Future-Proofed Interactive Toolset'],
+      ['12','Emard, Champlin and Runolfsdottir','Devolved Needs-Based Capability'],
+      ['13','Fritsch, Cronin and Wolff','Open-Source 3Rdgeneration Website'],
+      ['14','Borer LLC','Profit-Focused Incremental Orchestration'],
+      ['15','Emmerich-Ankunding','User-Centric Stable Extranet'],
+      ['16','Willms-Abbott','Progressive Bandwidth-Monitored Access'],
+      ['17','Brekke PLC','Intuitive User-Facing Customerloyalty'],
+      ['18','Bins, Toy and Klocko','Integrated Assymetric Software'],
+      ['19','Hodkiewicz-Hayes','Programmable Systematic Securedline'],
+      ['20','Murphy, Lang and Ferry','Organized Explicit Access'],
     ].map(companyDetails => ({
       id: companyDetails[0],
       name: companyDetails[1],
       description: companyDetails[2],
-      status: companyDetails[3],
+      status: 'backlog',
     }));
   }
   renderSwimlane(name, clients, ref) {
